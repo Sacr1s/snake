58230fb3f2218e11f21e6fe9373394bb8e1f7540
Sacr1s <vovanter14@gmail.com>
2016-08-10 16:24:41 +0300

    Функция Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,10 +14,15 @@ namespace Snake
             int y1 = 3;
             char sym1 = '*';
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            Draw(x1, y1, sym1);
 
             Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
